a38162d2b38ae8869a3dc0db746872e8b8ec93ca
mellopedro <123947113+mellopedro@users.noreply.github.com>
2024-08-14 21:57:45 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,15 @@
+/*
+ * Lab. 1
+ * Exemplo 1
+ */
+
+#include <stdio.h>
+
+int main() {
+  // imprime mensagens na tela
+  printf("Mackenzie - FCI\n");
+  printf("Algoritmos e ");
+  printf("Prog. II\n");
+  printf("Linguagem C\n");
+  return 0;
+}
